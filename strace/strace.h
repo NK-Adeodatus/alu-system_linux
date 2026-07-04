e8839e759eecee79590804fede422c4d51aeecf0
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-07-04 19:38:23 +0200

    Add header file for strace functionality

---
+++ strace/strace.h
@@ -0,0 +1,17 @@
+#ifndef STRACE_H
+#define STRACE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+
+extern char **environ;
+
+void child_exec(char **argv);
+void parent_trace(pid_t child);
+
+#endif /* STRACE_H */
