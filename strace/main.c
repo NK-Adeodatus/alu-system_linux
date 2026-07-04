74dbffa7bf85ee2e7b1e4620cb093802768cffc8
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-07-04 19:38:59 +0200

    Add main function for syscall tracing
    Implement main function to trace syscalls of a command.

---
+++ strace/main.c
@@ -0,0 +1,35 @@
+#include "strace.h"
+
+/**
+ * main - entry point, traces the syscalls made by a given command
+ * @argc: argument count
+ * @argv: argument vector, argv[1] is the command to trace
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char **argv)
+{
+	pid_t child;
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
+		return (1);
+	}
+
+	setbuf(stdout, NULL);
+
+	child = fork();
+	if (child == -1)
+	{
+		perror("fork");
+		return (1);
+	}
+
+	if (child == 0)
+		child_exec(argv);
+	else
+		parent_trace(child);
+
+	return (0);
+}
