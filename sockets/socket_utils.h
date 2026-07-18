2c134ca8757eb1209f7f4d2378115f32d929b150
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-07-18 20:38:06 +0200

    Add function to accept connection with IP address

--- sockets/socket_utils.h
+++ sockets/socket_utils.h
@@ -1,8 +1,11 @@
 #ifndef SOCKET_UTILS_H
 #define SOCKET_UTILS_H
 
+#include <stddef.h>
+
 int create_socket(void);
 void bind_socket(int sockfd, int port);
 int accept_connection(int sockfd);
+int accept_connection_with_ip(int sockfd, char *ip_str, size_t size);
 
 #endif /* SOCKET_UTILS_H */
