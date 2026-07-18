36bd62c890ff1a852ea112c346c80b6af5c8b9b5
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-07-18 19:41:23 +0200

    Add todo_store.h header file for todo item structure

---
+++ sockets/todo_store.h
@@ -0,0 +1,22 @@
+#ifndef TODO_STORE_H
+#define TODO_STORE_H
+
+/**
+ * struct todo_s - A single todo item
+ * @id: Positive integer identifier, starting from 0
+ * @title: The todo's title
+ * @description: The todo's description
+ * @next: Pointer to the next todo in the list
+ */
+typedef struct todo_s
+{
+	int id;
+	char *title;
+	char *description;
+	struct todo_s *next;
+} todo_t;
+
+todo_t *todo_create(const char *title, const char *description);
+char *todo_to_json(const todo_t *todo);
+
+#endif /* TODO_STORE_H */
