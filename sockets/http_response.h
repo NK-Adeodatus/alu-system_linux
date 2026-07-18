2429d87a12fcdb0aa1574a530d19b5c6c29aa08b
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-07-18 20:35:57 +0200

    Add new response functions to http_response.h

--- sockets/http_response.h
+++ sockets/http_response.h
@@ -2,5 +2,9 @@
 #define HTTP_RESPONSE_H
 
 void send_ok_response(int client_fd);
+void send_status_response(int client_fd, int status_code,
+			   const char *status_text);
+void send_json_response(int client_fd, int status_code,
+			 const char *status_text, const char *json);
 
 #endif /* HTTP_RESPONSE_H */
